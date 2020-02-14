f3fd22b4c597bca4b3f111951ceb408665cb31a6
Ramon.GallegoSimon <ramon.gallegosimon@nwclm04073116.nmfs.local>
2020-02-14 14:45:22 -0800

    Install githubs

--- installing.all.r.packages.R
+++ installing.all.r.packages.R
@@ -4,8 +4,30 @@ str(all.packags)
 
 install.packages(all.packags)
 
+library(tidyverse)
 tibble(libs = installed.packages()[,1]) -> all.in.new.installation
 
-library(tidyverse)
+
 tibble::tibble(all.libs = all.packags[,1]) %>%
-  filter (!all.libs %in% all.in.new.installation$libs )
+  filter (!all.libs %in% all.in.new.installation$libs ) %>% 
+  pull %>% 
+  map(install.packages)
+
+tibble::tibble(all.libs = all.packags[,1]) %>%
+  filter (!all.libs %in% all.in.new.installation$libs ) %>% 
+  pull %>% 
+  map(BiocManager::install)
+
+devtools::install_github("fishsciences/artemis", build_vignettes = TRUE)
+
+devtools::install_github("jiho/castr")
+
+devtools::install_github("bryandmartin/corncob")
+
+devtools::install_github("adw96/breakaway")
+devtools::install_github("adw96/DivNet")
+
+install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+
+devtools::install_github("ropenscilabs/roomba")
+
