9f571c0691f547f21c5ad3f21cfddd3786f3b3f2
ramongallego <ramongallego@gmail.com>
2020-02-15 14:31:36 -0800

    Update installing.all.r.packages.R

--- installing.all.r.packages.R
+++ installing.all.r.packages.R
@@ -1,22 +1,26 @@
+# Load the list of all packages I have installed in my previous R version
+
 all.packags <- readRDS("all.R.packages.installed.rds")
 
 str(all.packags)
 
+# Round 1: install from CRAN 
 install.packages(all.packags)
 
+# This should have done most of the job
+
 library(tidyverse)
+# Check what we have succeeded on
 tibble(libs = installed.packages()[,1]) -> all.in.new.installation
 
-
-tibble::tibble(all.libs = all.packags[,1]) %>%
-  filter (!all.libs %in% all.in.new.installation$libs ) %>% 
-  pull %>% 
-  map(install.packages)
+# Round 2: Bioconductor packages
 
 tibble::tibble(all.libs = all.packags[,1]) %>%
   filter (!all.libs %in% all.in.new.installation$libs ) %>% 
   pull %>% 
   map(BiocManager::install)
+
+# Round 3 : Github repos - 
 
 devtools::install_github("fishsciences/artemis", build_vignettes = TRUE)
 
